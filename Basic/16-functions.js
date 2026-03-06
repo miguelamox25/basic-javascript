8e3b09078a5cb0a1291f2bf81134f668b41551f1
stoker <miguelamox20@gmail.com>
2026-03-05 19:12:33 -0600

    commit 7

--- Basic/16-functions.js
+++ Basic/16-functions.js
@@ -39,5 +39,33 @@ const funcion1 = (nombre, apellido) => {
 funcion1('Fermin','Vela')
 
 // Se emplea mejor cuando se presenta en una sola linea
-const funcion2 = (nombre, apellido) => console.log(`Hola ${nombre} de apellido ${apellido}`)
+const funcion2 = (nombre, apellido) => console.log(`Hola ${nombre} de apellido ${apellido} estamos en la funcion 2`)
+
 funcion2('Pedro','Ipablo')
+
+///////////// FUNCIONES ANIDADAS ///////////}
+function externa (){
+    console.log('Hola externa') // Se imprime el mensaje en externa
+
+    function interna(){
+        console.log('Hola interna') // Se imprime el mensaje en interna
+    }
+
+    // interna() // Sin invocar a la funcion interna, no se mostraria
+}
+
+externa() // Se invoca la funcion externa
+
+///////////////// FUNCIONES DE ORDEN SUPERIOR ///////////////////
+// En este caso mediante una funcion estamos involucrando no solo parametros,
+// si no, mas funciones. Debemos de pasar los parametros que la funcion necesita.
+function aplica (func, param1, param2){
+    func(param1, param2)
+}
+
+aplica(funcion2,'Guillermo', 'Deltoro')
+
+////////////////// FOR EACH ///////////////////////
+let vector = [12,13,14,15] // Necesitamos un vector 
+
+vector.forEach((value) => console.log(value))
