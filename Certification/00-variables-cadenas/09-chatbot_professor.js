cd8c8d46cd157370d4291a7718b12bd83705edf0
stoker <miguelamox20@gmail.com>
2026-03-07 23:21:50 -0600

    commit 9

---
+++ Certification/00-variables-cadenas/09-chatbot_professor.js
@@ -0,0 +1,18 @@
+console.log("Hi there!");
+
+const botName = "teacherBot";
+
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+const subject = "JavaScript";
+const topic = "strings";
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
+
+let strLengthIntro
+
+strLengthIntro =`He aquí un ejemplo de uso de la propiedad length en la palabra ${subject}.`
+
+console.log(strLengthIntro)
